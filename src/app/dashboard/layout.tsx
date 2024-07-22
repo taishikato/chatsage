913d80c8dc66958e4738de1e0b5f010cbddbf476
taishikato <taishi.k0903@gmail.com>
2024-07-22 12:36:21 +0900

    Refactor dashboard layout to include navigation links

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export default function DashboardLayout({
   children,
 }: Readonly<{
@@ -5,7 +7,26 @@ export default function DashboardLayout({
 }>) {
   return (
     <div className="flex min-h-[calc(100vh-56px)] w-full flex-col bg-muted/40">
-      {children}
+      <div className="flex w-full flex-col">
+        <main className="flex min-h-[calc(100vh_-_theme(spacing.16))] flex-1 flex-col gap-4 bg-muted/40 p-4 md:gap-8 md:p-10">
+          <div className="mx-auto grid w-full max-w-6xl gap-2">
+            <h1 className="text-3xl font-semibold">Dashbaord</h1>
+          </div>
+          <div className="mx-auto grid w-full max-w-6xl items-start gap-6 md:grid-cols-[180px_1fr] lg:grid-cols-[250px_1fr]">
+            <nav
+              className="grid gap-4 text-sm text-muted-foreground"
+              x-chunk="dashboard-04-chunk-0"
+            >
+              <Link href="#" className="font-semibold text-primary">
+                Log
+              </Link>
+              <Link href="#">Data sources</Link>
+              <Link href="#">Settings</Link>
+            </nav>
+            {children}
+          </div>
+        </main>
+      </div>
     </div>
   );
 }
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,4 +1,3 @@
-import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import {
   Card,
@@ -13,71 +12,49 @@ import { Input } from "@/components/ui/input";
 
 export default function DashboardPage() {
   return (
-    <div className="flex w-full flex-col">
-      <main className="flex min-h-[calc(100vh_-_theme(spacing.16))] flex-1 flex-col gap-4 bg-muted/40 p-4 md:gap-8 md:p-10">
-        <div className="mx-auto grid w-full max-w-6xl gap-2">
-          <h1 className="text-3xl font-semibold">Dashbaord</h1>
-        </div>
-        <div className="mx-auto grid w-full max-w-6xl items-start gap-6 md:grid-cols-[180px_1fr] lg:grid-cols-[250px_1fr]">
-          <nav
-            className="grid gap-4 text-sm text-muted-foreground"
-            x-chunk="dashboard-04-chunk-0"
-          >
-            <Link href="#" className="font-semibold text-primary">
-              Log
-            </Link>
-            <Link href="#">Data sources</Link>
-            <Link href="#">Settings</Link>
-          </nav>
-          <div className="grid gap-6">
-            <Card x-chunk="dashboard-04-chunk-1">
-              <CardHeader>
-                <CardTitle>Store Name</CardTitle>
-                <CardDescription>
-                  Used to identify your store in the marketplace.
-                </CardDescription>
-              </CardHeader>
-              <CardContent>
-                <form>
-                  <Input placeholder="Store Name" />
-                </form>
-              </CardContent>
-              <CardFooter className="border-t px-6 py-4">
-                <Button>Save</Button>
-              </CardFooter>
-            </Card>
-            <Card x-chunk="dashboard-04-chunk-2">
-              <CardHeader>
-                <CardTitle>Plugins Directory</CardTitle>
-                <CardDescription>
-                  The directory within your project, in which your plugins are
-                  located.
-                </CardDescription>
-              </CardHeader>
-              <CardContent>
-                <form className="flex flex-col gap-4">
-                  <Input
-                    placeholder="Project Name"
-                    defaultValue="/content/plugins"
-                  />
-                  <div className="flex items-center space-x-2">
-                    <Checkbox id="include" defaultChecked />
-                    <label
-                      htmlFor="include"
-                      className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-                    >
-                      Allow administrators to change the directory.
-                    </label>
-                  </div>
-                </form>
-              </CardContent>
-              <CardFooter className="border-t px-6 py-4">
-                <Button>Save</Button>
-              </CardFooter>
-            </Card>
-          </div>
-        </div>
-      </main>
+    <div className="grid gap-6">
+      <Card x-chunk="dashboard-04-chunk-1">
+        <CardHeader>
+          <CardTitle>Store Name</CardTitle>
+          <CardDescription>
+            Used to identify your store in the marketplace.
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form>
+            <Input placeholder="Store Name" />
+          </form>
+        </CardContent>
+        <CardFooter className="border-t px-6 py-4">
+          <Button>Save</Button>
+        </CardFooter>
+      </Card>
+      <Card x-chunk="dashboard-04-chunk-2">
+        <CardHeader>
+          <CardTitle>Plugins Directory</CardTitle>
+          <CardDescription>
+            The directory within your project, in which your plugins are
+            located.
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form className="flex flex-col gap-4">
+            <Input placeholder="Project Name" defaultValue="/content/plugins" />
+            <div className="flex items-center space-x-2">
+              <Checkbox id="include" defaultChecked />
+              <label
+                htmlFor="include"
+                className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+              >
+                Allow administrators to change the directory.
+              </label>
+            </div>
+          </form>
+        </CardContent>
+        <CardFooter className="border-t px-6 py-4">
+          <Button>Save</Button>
+        </CardFooter>
+      </Card>
     </div>
   );
 }
