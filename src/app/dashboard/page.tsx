b677bc1afbda3d6051858b8252bb4406f28aecaa
taishikato <taishi.k0903@gmail.com>
2024-07-23 11:08:08 +0900

    Update dashboard page layout and content structure

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,59 +1,31 @@
-import { Button } from "@/components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import { Checkbox } from "@/components/ui/checkbox";
-import { Input } from "@/components/ui/input";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 
 export default function DashboardPage() {
   return (
     <div className="grid gap-6">
-      <Card x-chunk="dashboard-04-chunk-1">
+      <Card>
         <CardHeader>
-          <CardTitle>Store Name</CardTitle>
-          <CardDescription>
-            Used to identify your store in the marketplace.
-          </CardDescription>
+          <CardTitle>Chat logs</CardTitle>
         </CardHeader>
         <CardContent>
-          <form>
-            <Input placeholder="Store Name" />
-          </form>
-        </CardContent>
-        <CardFooter className="border-t px-6 py-4">
-          <Button>Save</Button>
-        </CardFooter>
-      </Card>
-      <Card x-chunk="dashboard-04-chunk-2">
-        <CardHeader>
-          <CardTitle>Plugins Directory</CardTitle>
-          <CardDescription>
-            The directory within your project, in which your plugins are
-            located.
-          </CardDescription>
-        </CardHeader>
-        <CardContent>
-          <form className="flex flex-col gap-4">
-            <Input placeholder="Project Name" defaultValue="/content/plugins" />
-            <div className="flex items-center space-x-2">
-              <Checkbox id="include" defaultChecked />
-              <label
-                htmlFor="include"
-                className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-              >
-                Allow administrators to change the directory.
-              </label>
+          <div className="text-sm">
+            <div className="space-y-3 p-4 rounded-xl cursor-pointer hover:bg-secondary">
+              <div className="text-secondary-foreground/50 flex items-center justify-between">
+                <span>Hi, what is the price?</span>
+                <span>4 hours ago</span>
+              </div>
+              <div>Here is our pricing.</div>
             </div>
-          </form>
+
+            <div className="space-y-3 p-4 rounded-xl cursor-pointer hover:bg-secondary">
+              <div className="text-secondary-foreground/50 flex items-center justify-between">
+                <span>Hi, what is the price?</span>
+                <span>4 hours ago</span>
+              </div>
+              <div>Here is our pricing.</div>
+            </div>
+          </div>
         </CardContent>
-        <CardFooter className="border-t px-6 py-4">
-          <Button>Save</Button>
-        </CardFooter>
       </Card>
     </div>
   );
