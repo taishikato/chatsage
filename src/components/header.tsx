569168fbb367303ac5c1992ed9c857ddd9a54461
taishikato <taishi.k0903@gmail.com>
2024-07-24 10:43:27 +0900

    Revert "Remove authentication logic from layout and header"
    This reverts commit 007bb1d7e1d3976a783ac217378b2679913b8ee4.

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,18 +2,24 @@
 
 import Link from "next/link";
 import { Button } from "./ui/button";
+import { logout } from "@/actions/logout";
 
-export const Header = () => {
+export const Header = ({ isLoggedIn }: { isLoggedIn: boolean }) => {
   return (
     <header className="border-b h-[56px]">
       <div className="max-w-7xl mx-auto flex items-center px-6 py-2">
         <h1 className="mr-auto">
           <Link href="/">SupaChat</Link>
         </h1>
-
-        <Button asChild>
-          <Link href="/login">Login</Link>
-        </Button>
+        {isLoggedIn ? (
+          <form>
+            <Button formAction={logout}>Logout</Button>
+          </form>
+        ) : (
+          <Button asChild>
+            <Link href="/login">Login</Link>
+          </Button>
+        )}
       </div>
     </header>
   );
