1b12a63e563d05be53662664b5e32d16cd2bc513
taishikato <taishi.k0903@gmail.com>
2024-07-26 22:13:30 +0900

    Update CORS headers to allow more methods

--- src/app/api/get-chatbot-status/[id]/route.ts
+++ src/app/api/get-chatbot-status/[id]/route.ts
@@ -9,7 +9,7 @@ export async function GET(
 
   const headers = {
     "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Methods": "GET,OPTIONS",
+    "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
     "Access-Control-Allow-Headers": "Content-Type, Authorization",
   };
 
