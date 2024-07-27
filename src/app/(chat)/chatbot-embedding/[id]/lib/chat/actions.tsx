35871b07716da9fa2b267b51d4dc0ab602022556
taishikato <taishi.k0903@gmail.com>
2024-07-27 15:57:10 +0900

    remove commented-out auth logic from actions.tsx

--- src/app/(chat)/chatbot-embedding/[id]/lib/chat/actions.tsx
+++ src/app/(chat)/chatbot-embedding/[id]/lib/chat/actions.tsx
@@ -188,56 +188,10 @@ export const AI = createAI<AIState, UIState>({
   onGetUIState: async () => {
     "use server";
 
-    // const session = await auth()
-
-    // console.log({ session })
-
-    // if (session && session.user) {
-    //   const aiState = getAIState() as Chat
-
-    //   if (aiState) {
-    //     const uiState = getUIStateFromAIState(aiState)
-    //     return uiState
-    //   }
-    // } else {
-    //   return
-    // }
-
     const aiState = getAIState() as Chat;
     const uiState = getUIStateFromAIState(aiState);
 
     return uiState;
-  },
-  onSetAIState: async ({ state }) => {
-    "use server";
-
-    // const session = await auth()
-
-    // if (session && session.user) {
-    //   const { chatId, messages } = state
-
-    //   const createdAt = new Date()
-    //   const userId = session.user.id as string
-    //   const path = `/chat/${chatId}`
-
-    //   const firstMessageContent = messages[0].content as string
-    //   const title = firstMessageContent.substring(0, 100)
-
-    //   const chat: Chat = {
-    //     id: chatId,
-    //     title,
-    //     userId,
-    //     createdAt,
-    //     messages,
-    //     path
-    //   }
-
-    //   await saveChat(chat)
-    // } else {
-    //   return
-    // }
-
-    return;
   },
 });
 
