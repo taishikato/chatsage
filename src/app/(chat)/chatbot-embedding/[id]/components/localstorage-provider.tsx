db2279ca4b95976aea34c18e3416c8396df44e2e
taishikato <taishi.k0903@gmail.com>
2024-07-27 10:23:48 +0900

    comment out postMessage

--- src/app/(chat)/chatbot-embedding/[id]/components/localstorage-provider.tsx
+++ src/app/(chat)/chatbot-embedding/[id]/components/localstorage-provider.tsx
@@ -36,26 +36,26 @@ export const LocalStorageProvider = ({
     localStorage.setItem(localStorageKeyForConversationId, conversationId);
   }, [conversationId]);
 
-  useEffect(() => {
-    window.parent.postMessage(
-      {
-        type: "setItem",
-        key: localStorageKeyForConversationId,
-        value: conversationId,
-      },
-      "*"
-    );
+  // useEffect(() => {
+  //   window.parent.postMessage(
+  //     {
+  //       type: "setItem",
+  //       key: localStorageKeyForConversationId,
+  //       value: conversationId,
+  //     },
+  //     "*"
+  //   );
 
-    // const handleMessage = (event: any) => {
-    //   if (event.data.type === "GET_LOCAL_STORAGE") {
-    //     const value = localStorage.getItem(event.data.key);
-    //     window.parent.postMessage({ type: "LOCAL_STORAGE_VALUE", value }, "*");
-    //   }
-    // };
+  // const handleMessage = (event: any) => {
+  //   if (event.data.type === "GET_LOCAL_STORAGE") {
+  //     const value = localStorage.getItem(event.data.key);
+  //     window.parent.postMessage({ type: "LOCAL_STORAGE_VALUE", value }, "*");
+  //   }
+  // };
 
-    // window.addEventListener("message", handleMessage);
-    // return () => window.removeEventListener("message", handleMessage);
-  }, [localStorageKeyForConversationId, conversationId]);
+  // window.addEventListener("message", handleMessage);
+  // return () => window.removeEventListener("message", handleMessage);
+  // }, [localStorageKeyForConversationId, conversationId]);
 
   return (
     <LocalStorageContext.Provider
