663f70b8fc15c50dc070836464281f55da179352
taishikato <taishi.k0903@gmail.com>
2024-07-29 16:04:21 +0900

    set cookie to request api

--- src/actions/scrape.ts
+++ src/actions/scrape.ts
@@ -2,6 +2,7 @@
 
 import { APP_URL } from "@/lib/consts";
 import { createClient } from "@/lib/supabase/server";
+import { cookies } from "next/headers";
 
 import axios from "redaxios";
 
@@ -11,6 +12,8 @@ export const scrape = async (url: string) => {
     data: { user },
   } = await supabase.auth.getUser();
 
+  const cookieStore = cookies();
+
   const { data: project } = await supabase
     .from("chatbots")
     .select("internal_id")
@@ -18,10 +21,22 @@ export const scrape = async (url: string) => {
       user_auth_id: user!.id,
     });
 
-  await axios.post(`${APP_URL}/api/protetced/scrape`, {
-    url,
-    chatbotInternalId: project![0].internal_id,
-  });
+  try {
+    await axios.post(
+      `${APP_URL}/api/protected/scrape`,
+      {
+        url,
+        chatbotInternalId: project![0].internal_id,
+      },
+      {
+        headers: {
+          Cookie: cookieStore.toString(),
+        },
+      }
+    );
+  } catch (err) {
+    console.error(err);
+  }
 
   return {
     success: true,
