7566b88431f9590aba4b88238fd23d95a32c9148
taishikato <taishi.k0903@gmail.com>
2024-07-29 10:31:40 +0900

    APP_NAME = Chatsage

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,13 +2,14 @@
 
 import Link from "next/link";
 import { Button } from "./ui/button";
+import { APP_NAME } from "@/lib/consts";
 
 export const Header = ({ isLoggedIn }: { isLoggedIn: boolean }) => {
   return (
     <header className="border-b h-[56px]">
       <div className="max-w-7xl mx-auto flex items-center px-6 py-2 h-full">
         <h1 className="mr-auto">
-          <Link href="/">SupaChat</Link>
+          <Link href="/">{APP_NAME}</Link>
         </h1>
         {!isLoggedIn && (
           <Button asChild>
--- src/lib/consts.ts
+++ src/lib/consts.ts
@@ -3,3 +3,5 @@ const isProd = process.env.NODE_ENV === "production";
 export const APP_URL = isProd
   ? process.env.NEXT_PUBLIC_APP_URL
   : "http://localhost:3000";
+
+export const APP_NAME = "Chatsage";
