ca778cdb83c97187249e0b41d937fc117b0a5de0
taishikato <taishi.k0903@gmail.com>
2024-07-30 09:26:19 +0900

    Update dev URL to localhost in consts.ts

--- src/lib/consts.ts
+++ src/lib/consts.ts
@@ -2,6 +2,6 @@ const isProd = process.env.NODE_ENV === "production";
 
 export const APP_URL = isProd
   ? process.env.NEXT_PUBLIC_APP_URL
-  : process.env.NEXT_PUBLIC_APP_DEV_URL;
+  : "http://localhost:3000";
 
 export const APP_NAME = "Chatsage";
