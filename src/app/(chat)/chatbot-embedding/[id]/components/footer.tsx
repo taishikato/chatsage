851edba1e9676d07297586e5bef2fa86ac3e77c3
taishikato <taishi.k0903@gmail.com>
2024-08-02 14:56:01 +0900

    add logo to the chatbot footer

--- src/app/(chat)/chatbot-embedding/[id]/components/footer.tsx
+++ src/app/(chat)/chatbot-embedding/[id]/components/footer.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 import { cn } from "@/lib/utils";
-import { APP_NAME } from "@/lib/consts";
+import { APP_NAME, APP_URL } from "@/lib/consts";
 
 export function FooterText({ className, ...props }: React.ComponentProps<"p">) {
   return (
@@ -12,7 +12,13 @@ export function FooterText({ className, ...props }: React.ComponentProps<"p">) {
       )}
       {...props}
     >
-      Powered by {APP_NAME}
+      <img
+        src="/chatsage-chat-footer-logo.svg"
+        className="size-4 grayscale rounded-md mr-1 opacity-50"
+      />
+      <a target="_blank" href={APP_URL}>
+        Powered by {APP_NAME}
+      </a>
     </footer>
   );
 }
