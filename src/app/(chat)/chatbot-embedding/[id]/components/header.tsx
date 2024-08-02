6369da230750a42d508728eff6356450d17728b8
taishikato <taishi.k0903@gmail.com>
2024-08-02 14:58:14 +0900

    Increase header padding in chatbot component

--- src/app/(chat)/chatbot-embedding/[id]/components/header.tsx
+++ src/app/(chat)/chatbot-embedding/[id]/components/header.tsx
@@ -9,7 +9,7 @@ export const Header = ({ chatBotName }: { chatBotName: string | null }) => {
   const { setValue } = useLocalStorage();
 
   return (
-    <header className="sticky top-0 z-50 flex items-center justify-between w-full h-16 px-4 border-b shrink-0 bg-gradient-to-b from-background/10 via-background/50 to-background/80 backdrop-blur-xl">
+    <header className="sticky top-0 z-50 flex items-center justify-between w-full h-16 px-6 border-b shrink-0 bg-gradient-to-b from-background/10 via-background/50 to-background/80 backdrop-blur-xl">
       <div className="flex items-center w-full">
         <h1 className="font-semibold text-sm mr-auto">
           {chatBotName ?? "No name"}
