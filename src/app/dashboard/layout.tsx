ada745c173e8fd3df5b08656671083d3ad502b2f
Viktor Szépe <viktor@szepe.net>
2024-08-06 20:07:43 +0200

    Fix a typo in layout

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -21,7 +21,7 @@ export default async function DashboardLayout({
           <div className="flex w-full flex-col">
             <main className="flex w-full min-h-[calc(100vh_-_theme(spacing.16))] flex-1 flex-col gap-6 bg-muted/40 p-4 md:gap-8 md:py-10">
               <div className="mx-auto grid w-full max-w-7xl md:px-6">
-                <h1 className="text-3xl font-semibold">Dashbaord</h1>
+                <h1 className="text-3xl font-semibold">Dashboard</h1>
               </div>
               <div className="mx-auto w-full md:max-w-7xl md:px-6 items-start gap-6 flex flex-col md:flex-row">
                 <LayoutNav />
