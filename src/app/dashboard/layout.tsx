6c1c8f09bc3e144d36a230f98e39e8c9a2e9a969
taishikato <taishi.k0903@gmail.com>
2024-08-06 11:18:19 +0900

    Update layout dimensions and padding in dashboard

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -20,10 +20,10 @@ export default async function DashboardLayout({
         <div className="flex min-h-[calc(100vh-64px)] w-full flex-col bg-muted/40">
           <div className="flex w-full flex-col">
             <main className="flex min-h-[calc(100vh_-_theme(spacing.16))] flex-1 flex-col gap-6 bg-muted/40 p-4 md:gap-8 md:py-10">
-              <div className="mx-auto grid w-full max-w-6xl gap-2">
+              <div className="mx-auto grid w-full max-w-7xl px-6">
                 <h1 className="text-3xl font-semibold">Dashbaord</h1>
               </div>
-              <div className="mx-auto grid w-full max-w-6xl items-start gap-6 md:grid-cols-[180px_1fr] lg:grid-cols-[250px_1fr]">
+              <div className="mx-auto grid w-full max-w-7xl px-6 items-start gap-6 md:grid-cols-[180px_1fr] lg:grid-cols-[250px_1fr]">
                 <LayoutNav />
                 {children}
               </div>
