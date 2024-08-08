f114ceb09842f02cccf75c678e64b068994e4ea3
taishikato <taishi.k0903@gmail.com>
2024-08-08 11:41:30 +0900

    add tab ui

--- src/app/dashboard/sources/page.tsx
+++ src/app/dashboard/sources/page.tsx
@@ -7,6 +7,7 @@ import {
 import { createClient } from "@/lib/supabase/server";
 import { CrawlForm } from "./_components/crawl-form";
 import { SitemapForm } from "./_components/sitemap-form";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 // to handle scraping server actions
 export const maxDuration = 30;
@@ -26,34 +27,76 @@ export default async function SourcesPage() {
 
   return (
     <div className="w-full">
-      <Card className="">
-        <CardHeader>
-          <CardTitle>Sources</CardTitle>
-        </CardHeader>
-        <CardContent>
-          <section>
-            <h2 className="text-lg font-semibold mb-4">Crawl</h2>
-            <CrawlForm />
-          </section>
+      <Tabs defaultValue="account" className="w-full">
+        <TabsList className="w-full">
+          <TabsTrigger value="website" className="flex-1">
+            Website
+          </TabsTrigger>
+          <TabsTrigger value="qanda" className="flex-1">
+            Q&A
+          </TabsTrigger>
+        </TabsList>
+        <TabsContent value="website">
+          <Card className="">
+            <CardHeader>
+              <CardTitle>Website</CardTitle>
+            </CardHeader>
+            <CardContent>
+              <section>
+                <h2 className="text-lg font-semibold mb-4">Crawl</h2>
+                <CrawlForm />
+              </section>
 
-          <div className="w-full my-8 text-center text-muted-foreground">
-            OR
-          </div>
+              <div className="w-full my-8 text-center text-muted-foreground">
+                OR
+              </div>
 
-          <section>
-            <h2 className="text-lg font-semibold mb-4">Sitemap</h2>
-            <SitemapForm />
-          </section>
+              <section>
+                <h2 className="text-lg font-semibold mb-4">Sitemap</h2>
+                <SitemapForm />
+              </section>
 
-          <Separator className="my-12" />
+              <Separator className="my-12" />
 
-          <FetchedSourcesSection
-            sources={
-              projectAndUrls ? (projectAndUrls[0].urls as Source[]) : null
-            }
-          />
-        </CardContent>
-      </Card>
+              <FetchedSourcesSection
+                sources={
+                  projectAndUrls ? (projectAndUrls[0].urls as Source[]) : null
+                }
+              />
+            </CardContent>
+          </Card>
+        </TabsContent>
+        <TabsContent value="qanda">
+          <Card className="">
+            <CardHeader>
+              <CardTitle>Q&A</CardTitle>
+            </CardHeader>
+            <CardContent>
+              <section>
+                <h2 className="text-lg font-semibold mb-4">Crawl</h2>
+                <CrawlForm />
+              </section>
+
+              <div className="w-full my-8 text-center text-muted-foreground">
+                OR
+              </div>
+
+              <section>
+                <h2 className="text-lg font-semibold mb-4">Sitemap</h2>
+                <SitemapForm />
+              </section>
+
+              <Separator className="my-12" />
+
+              <FetchedSourcesSection
+                sources={
+                  projectAndUrls ? (projectAndUrls[0].urls as Source[]) : null
+                }
+              />
+            </CardContent>
+          </Card>
+        </TabsContent>
+      </Tabs>
     </div>
   );
 }
